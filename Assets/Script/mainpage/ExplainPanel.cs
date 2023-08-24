75407da2d1203c6078ffa88a0281e4da99b30c31
Gayeong Lee <gayyeong@naver.com>
2023-08-25 02:24:59 +0900

    store0224

---
+++ Assets/Script/mainpage/ExplainPanel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplainPanel : MonoBehaviour
+{
+    public GameObject[] panelButton, ExpPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextButtonClick(int i)
+    {
+        switch (i)
+        {
+          /*  case 0: tabNum = 0; break;
+            case "Bag": tabNum = 1; break;*/
+        }
+    }
+}
